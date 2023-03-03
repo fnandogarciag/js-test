a329440174ccb73c617cd5d3861e45b71721b006
Fernando Garcia <fernandgarcia97@hotmail.com>
2023-03-03 18:48:11 -0500

    first commit

---
+++ demos/src/02-math.test.js
@@ -0,0 +1,24 @@
+const { sum, multiply, divide } = require('./02-math');
+
+describe('Test for math', () => {
+  describe('tests for sum', () => {
+    test('adds 1 + 3 should be 4', () => {
+      const rta = sum(1, 3);
+      expect(rta).toBe(4);
+    });
+  });
+  describe('tests for multiply', () => {
+    test('should be 4', () => {
+      const rta = multiply(1, 4);
+      expect(rta).toBe(4);
+    });
+  });
+  describe('tests for divide', () => {
+    test('should divide', () => {
+      const rta = divide(6, 0);
+      expect(rta).toBeNull();
+      const rta2 = divide(5, 2);
+      expect(rta2).toBe(2.5);
+    });
+  });
+});
